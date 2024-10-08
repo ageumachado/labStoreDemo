299ad1ee21603c5f8ce76030ded34bf287900fa5
ageu <ageu.machado@db1.com.br>
2024-10-08 18:04:38 -0300

    Implementaçao do indexeddb

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
+
+const dbConfig: DBConfig = {
+  name: 'storeDB',
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: 'loja',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [],
+    },
+  ],
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    importProvidersFrom(NgxIndexedDBModule.forRoot(dbConfig)),
   ],
 };
--- src/app/services/loja.service.ts
+++ src/app/services/loja.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
+import { Loja } from '../models/loja';
+import { EMPTY, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LojaService {
+  private _storeName: string = 'loja';
+  constructor(private dbService: NgxIndexedDBService) {}
 
-  constructor() { }
+  addOrUpdate(entidade: Loja) {
+    return this.dbService.update(this._storeName, entidade);
+  }
+
+  exists(id: string) {
+    return this.dbService
+      .count(this._storeName, id)
+      .pipe(map((result) => result > 0));
+  }
+
+  getAll() {
+    return this.dbService.getAll<Loja | null>(this._storeName);
+  }
+
+  getByKey(id?: string) {
+    if (!id) return EMPTY;
+    return this.dbService.getByKey<Loja | null>(this._storeName, id);
+  }
+
+  delete(id?: string) {
+    if (!id) return EMPTY;
+    return this.dbService.delete(this._storeName, id);
+  }
+
+  clean() {
+    return this.dbService.clear(this._storeName);
+  }
 }
